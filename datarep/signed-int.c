62ead27face13c7d25b10f363dc10cf7728dac5b
Malte Schwarzkopf <malte@cs.brown.edu>
2024-02-13 11:21:40 -0500

    More Lecture 6 tweaks

--- datarep/signed-int.c
+++ datarep/signed-int.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
 #include "hexdump.h"
 
 void f() {
-  int i1 = 1;
-  int i2 = -1;
+  char i1 = 1;
+  char i2 = -1;
 
   hexdump(&i1, sizeof(i1));
   hexdump(&i2, sizeof(i2));
 
-  int i3 = i1 + i2;
+  char i3 = i1 + i2;
   hexdump(&i3, sizeof(i3));
 }
 
